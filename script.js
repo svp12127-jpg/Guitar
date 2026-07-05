3aa6c58dd8a42777c0842613a56f2c7d99a78a24
Srivaishnavi P <260255967+svp12127-jpg@users.noreply.github.com>
2026-07-05 18:32:16 +0530

    mic

--- script.js
+++ script.js
@@ -4,5 +4,14 @@ startBtn.addEventListener('click', async () => {
   const source = audiocontext.createMediaStreamSource(stream);
   const analyser = audiocontext.createAnalyser();
   source.connect(analyser);
+  analyser.fftSize = 2048;
+  const bufferLength = analyser.fftSize;
+  const dataArray = new Float32Array(bufferLength);
   status.textContent = "Mic connected!";
+
+  function update() {
+    analyser.getFloatTimeDomainData(dataArray);
+    requestAnimationFrame(update);
+    }
+  update();
 });
